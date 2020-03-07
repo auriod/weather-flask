7473a010d794265659817e8f54d438cf29bf09a1
auriod <kiefanov1@gmail.com>
2020-03-07 16:25:35 +0200

    Реализована валидация данных нового пользователя на стороне сервера

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,5 @@
 import functools
+import re
 
 from flask import Blueprint
 from flask import flash
@@ -27,12 +28,27 @@ def login_required(view):
     return wrapped_view
 
 
+def is_valid_name(name):
+    """Валидация имени и фамилии. Одно слово, без пробелов, только кириллица"""
+    reg = r"^[а-я]+$"
+    return bool(re.match(reg, name.lower()))
+
+
+def is_valid_email(email):
+    """Валидация email по шаблону name@example.com"""
+    reg = r'^[a-z0-9_-]+@[a-z]+\.[a-z]{2,6}$'
+    return bool(re.match(reg, email, flags=re.I))
+
+
+def is_valid_bisday(birsday):
+    """Валидация даты рождения. Формат ГГГГ-ММ-ДД"""
+    reg = r'^\d{4}-\d\d-\d\d$'
+    return bool(re.match(reg, birsday))
+
 
 # регистрация нового пользователя
 @auth_page.route("/register", methods=("GET", "POST"))
 def register():
-
-    # !!! ДОДЕЛАТЬ ВАЛИДАЦИЮ
 
     if request.method == "POST":
         db = get_db()
@@ -40,34 +56,33 @@ def register():
 
         first_name = request.form["first_name"].strip()
         last_name = request.form["last_name"].strip()
-        birsday = request.form["birsday"]
         email = request.form["email"].strip()
+        birsday = request.form["birsday"]
         gender = request.form['gender']
 
-        # if not birsday: birsday = 'NULL'
-        # if not birsday: birsday = 'NULL'
 
-        if not first_name:
-            message = "Введите имя"
-        if not last_name:
-            message = "Введите фамилию"
-        if not email:
-            message = "Введите email"
+        if not is_valid_name(first_name):
+            message = "Некорректное имя"
+        if not is_valid_name(last_name):
+            message = "Некорректная фамилия"
+        if not is_valid_email(email):
+            message = "Некорректный email"
         if not birsday:
             birsday = None
+        elif not is_valid_bisday(birsday):
+            message = 'Некорректная дата рождения' 
         if not gender:
             gender = None
         
-        # if message is not None:
-        #     flash(message)
-        #     return redirect(url_for('index'))
-        
+        if message is not None:
+            flash(messga)
+            return redirect(url_for('index'))
+
         # проверка наличия введенного email в БД
         # если email есть, Пользователь считается зарестрированным
         if not db.is_exist_email(email):
             try:
                 db.add_new_user((first_name, last_name, email, gender, birsday))
-                # session['data'] = (first_name, last_name, email, gender, birsday, repr(type(birsday)))
             except Exception as err:
                 flash(repr(err))
                 return redirect(url_for('index'))
@@ -75,6 +90,9 @@ def register():
             
                
         session['logged_in'] = True
-        session['user'] = first_name + ' ' + last_name
+        session['user'] = {
+            'username': ' '.join([last_name, first_name]),
+            'email': email,
+        }
 
         return redirect(url_for('index'))
